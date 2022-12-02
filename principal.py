841e73dfeb84fdd946abf64e5a3552810dc1818d
kolades <117230554+kolades@users.noreply.github.com>
2022-12-02 18:42:37 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -36,7 +36,7 @@ arvore.fit(features_treino,classes_treino)
 #testando a arvore treinada
 #resultado = arvore.predict(features_teste)
 
-st.markdown("coracao-batendo")
+st.markdown("coracao-batendo.gif")
 st.markdown("<h1 style='text-align: center; color: green;'>Previsão de Doença Cardiaca</h1>", unsafe_allow_html=True)
 #Age = st.number_input('Digite a idade do paciente:', min_value=1,max_value=150)
 Age = st.slider(
