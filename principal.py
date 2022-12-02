5a280731cf39e89fe90baa825faf3eb7c4676af7
kolades <117230554+kolades@users.noreply.github.com>
2022-12-01 21:07:33 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -39,7 +39,7 @@ arvore.fit(features_treino,classes_treino)
 st.markdown("<h1 style='text-align: center; color: green;'>Aplicativo IA - Previsão de Doença Cardiaca</h1>", unsafe_allow_html=True)
 #Age = st.number_input('Digite a idade do paciente:', min_value=1,max_value=150)
 Age = st.slider(
-            "# of results",
+            "Informe a idade do paciente:",
             min_value=1,
             max_value=30,
             value=10,
